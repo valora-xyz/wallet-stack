40e30d4b2e507fccd6ac5ef9d1ee68dc67ce9857
Tom McGuire <mcgtom10@gmail.com>
2026-04-30 11:33:07 -0700

    test(e2e): account test only on ios

--- apps/example/e2e/src/AccountManagement.spec.js
+++ apps/example/e2e/src/AccountManagement.spec.js
@@ -3,7 +3,7 @@ import Settings from './usecases/Settings'
 import Support from './usecases/Support'
 import { quickOnboarding } from './utils/utils'
 
-describe('Account', () => {
+describe('Account :ios:', () => {
   beforeAll(async () => {
     await quickOnboarding()
   })
